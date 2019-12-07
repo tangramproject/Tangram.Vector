6e987b0d6f6da4dafbff5d690db62bfd99583254
pingpongsneak <pingpong@getsneak.org>
2019-12-07 16:51:35 +0000

    MQTT client managed pub/sub.

---
+++ VectorContainers/Core.API/MQTT/Publisher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+
+namespace Core.API.MQTT
+{
+    public class Publisher
+    {
+        private readonly ulong id;
+        private readonly string host;
+        private readonly int port;
+        private readonly ILogger logger;
+        private readonly IManagedMqttClient client;
+
+        public Publisher(ulong id, string host, int port)
+        {
+            this.id = id;
+            this.host = host;
+            this.port = port;
+
+            var loggerFactory = new LoggerFactory();
+            logger = loggerFactory.CreateLogger<Publisher>();
+
+            client = new MqttFactory().CreateManagedMqttClient();
+
+            Start();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal void Start()
+        {
+            var options = new ManagedMqttClientOptionsBuilder()
+              .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+              .WithClientOptions(new MqttClientOptionsBuilder()
+                  .WithClientId($"Publisher-{id}")
+                  .WithTcpServer(host, port)
+                  .Build())
+              .Build();
+
+            client.StartAsync(options);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="topic"></param>
+        /// <param name="payload"></param>
+        public void Publish(string topic, byte[] payload)
+        {
+            try
+            {
+                var message = new MqttApplicationMessageBuilder()
+                   .WithTopic(topic)
+                   .WithPayload(payload)
+                   .Build();
+
+                client.PublishAsync(message);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"<<< Publisher.Publish >>>: {ex.ToString()}");
+            }
+        }
+    }
+}
---
+++ VectorContainers/Core.API/MQTT/Subscriber.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+
+namespace Core.API.MQTT
+{
+    public class Subscriber
+    {
+        public event EventHandler<MqttApplicationMessageReceivedEventArgs> MqttApplicationMessageReceived;
+
+        private readonly ulong id;
+        private readonly string host;
+        private readonly int port;
+        private readonly string topic;
+        private readonly ILogger logger;
+        private readonly IManagedMqttClient client;
+
+        public Subscriber(ulong id, string host, int port, string topic)
+        {
+            this.id = id;
+            this.host = host;
+            this.port = port;
+            this.topic = topic;
+
+            var loggerFactory = new LoggerFactory();
+            logger = loggerFactory.CreateLogger<Subscriber>();
+
+            client = new MqttFactory().CreateManagedMqttClient();
+
+            Start();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal void Start()
+        {
+            var options = new ManagedMqttClientOptionsBuilder()
+                .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                .WithClientOptions(new MqttClientOptionsBuilder()
+                    .WithClientId($"Subscriber-{id}")
+                    .WithTcpServer(host, port)
+                    .Build())
+                .Build();
+
+            client.UseApplicationMessageReceivedHandler(OnMessageReceived);
+            client.SubscribeAsync(topic);
+            client.StartAsync(options);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="arg"></param>
+        private void OnMessageReceived(MqttApplicationMessageReceivedEventArgs arg)
+        {
+            OnMqttApplicationMessageReceived(arg);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        protected virtual void OnMqttApplicationMessageReceived(MqttApplicationMessageReceivedEventArgs e)
+        {
+            if (MqttApplicationMessageReceived == null)
+            {
+                logger.LogWarning("<<< Subscriber.OnMqttApplicationMessageReceived >>>: Mqtt messages will not be received. Event handler delegate not defined.");
+            }
+
+            MqttApplicationMessageReceived?.Invoke(this, e);
+        }
+    }
+}

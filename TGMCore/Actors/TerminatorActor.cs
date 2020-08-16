036c64b8b323327646bfc1e56f46f8beaa10247c
pingpongsneak <matthew.hellyer@pm.me>
2020-08-16 23:52:46 +0100

    Restart actor system when node is put into quarantined.

---
+++ TGMCore/Actors/TerminatorActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Remote;
+using TGMCore.Services;
+
+namespace TGMCore.Actors
+{
+    public class TerminatorActor: ReceiveActor
+    {
+        private readonly IActorService _actorService;
+
+        public TerminatorActor(IActorService actorService)
+        {
+            _actorService = actorService;
+
+            ReceiveAsync<ThisActorSystemQuarantinedEvent>(async m =>
+            {
+                var shutdownTask = CoordinatedShutdown.Get(_actorService.Get).Run(CoordinatedShutdown.ClrExitReason.Instance);
+                await shutdownTask;
+
+                _actorService.Start();
+            });
+        }
+    }
+}
